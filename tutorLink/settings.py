a50f8d71b4bc21186619113f78704443b2bdd69b
Blankpro123 <Adrielcarperez@gmail.com>
2026-05-08 19:42:03 -0500

    deployment

--- tutorLink/settings.py
+++ tutorLink/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-z82wz95)oxbhorgb17_46cxs@u8-hd%+i31f--4rkr44vw_wd@'
 
-SECRET_KEY = os.environ.get('SECRET_KEY', default='unsecure-key'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='unsecure-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
